bd54b9303dbcab88e675d8c0cff8ef6e0b14ca73
Fabricio Castro <baptistafabricio87@gmail.com>
2020-11-14 17:48:07 -0300

    Project Created

---
+++ src/test/java/com/webservice/cursojava/CursojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservice.cursojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
